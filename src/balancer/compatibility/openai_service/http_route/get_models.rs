e85cee296db79e8bce7d5b946d407a4b48de3726
FuryFiber <flor.mertens@proximus.be>
2026-02-04 13:49:46 +0100

    read desired_state

--- src/balancer/compatibility/openai_service/http_route/get_models.rs
+++ src/balancer/compatibility/openai_service/http_route/get_models.rs
@@ -3,7 +3,7 @@ use std::time::UNIX_EPOCH;
 
 use actix_web::Error;
 use actix_web::HttpResponse;
-use actix_web::get;
+use actix_web::post;
 use actix_web::web;
 use anyhow::anyhow;
 use async_trait::async_trait;
@@ -12,15 +12,29 @@ use serde::Deserialize;
 use serde_json::json;
 use tokio_stream::StreamExt as _;
 
-use crate::balancer::compatibility::openai_service::app_data::AppData;
-
+use crate::balancer::management_service::app_data::AppData;
 
 pub fn register(cfg: &mut web::ServiceConfig) {
-    cfg.service(list_models);
+    cfg.service(respond);
 }
 
-#[get("/v1/models")]
-async fn list_models(app_data: web::Data<AppData>) -> Result<HttpResponse, Error> {
-    let response = "hello world!";
-    Ok(HttpResponse::Ok().json(response))
+fn current_timestamp() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs()
+}
+
+#[post("/v1/models")]
+async fn list_models(
+    app_data: web::Data<AppData>,
+) -> Result<HttpResponse, Error> {
+    let desired_state = app_data
+        .state_database
+        .read_balancer_desired_state()
+        .await
+        .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().json(desired_state))
+
 }
